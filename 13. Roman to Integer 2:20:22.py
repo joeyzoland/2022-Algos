e16eb7b3b80b68e8d0741469f947e1414f1ee9e4
Joseph Zoland <joeyzoland@gmail.com>
2022-02-23 14:19:36 -0800

    Adding 13. Roman to Integer 2:20:22.py

---
+++ 13. Roman to Integer 2:20:22.py
@@ -0,0 +1,82 @@
+"""
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9.
+X can be placed before L (50) and C (100) to make 40 and 90.
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+
+Constraints:
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+"""
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        #Initialize solution variable
+        #Create a dictionary of every symbol and it's value (e.g., I:1), as well as the special values (e.g., IV)
+        #Use while loop to iterate through input, left to right
+            #If you've got two values to check
+                #Check the numeral and the numeral to the right
+                    #If the numeral is special in dict
+                        #Add value to solution
+                    #Otherwise
+                        #Add normal value to solution
+            #If you've only got one value to check
+                #Add to solution variable
+        #Return solution variable
+        solution = 0
+        numeralDict = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        specialDict = {"IV":4, "IX":9, "XL":40, "XC":90, "CD":400, "CM":900}
+        i = 0
+        while i < (len(s) - 1):
+            if s[i:i+2] in specialDict:
+                solution += specialDict[s[i:i+2]]
+                i += 2
+            else:
+                solution += numeralDict[s[i]]
+                i += 1
+        if i == len(s) - 1:
+            solution += numeralDict[s[i]]
+        return solution
+
+solution = Solution()
+input = "MCMXCIV"
+print solution.romanToInt(input)
